8d371a2fe1f1917eb52668aafb4a9c91099d9836
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-04 19:55:08 -0300

    criado variaveis de estado principais para a aplicação

--- src/pay-take.js
+++ src/pay-take.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./pay-take.css";
 
 function PayTake() {
+    const [carrinho, setCarrinho] = useState({ produtos: [] });
+    const [exibirProdutos, setExibirProdutos] = useState(true);
+    const [exibirCheckout, setExibirCheckout] = useState(true);
+    const [total, setTotal] = useState("0,00");
+
     return (
         <div className="">
             <h1>Pay&#38;Take</h1>
