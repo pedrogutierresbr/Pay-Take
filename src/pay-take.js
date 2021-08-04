38d2f118c61c921a22f40f6c254d7b74aa8de581
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-04 20:08:10 -0300

    criado todos os componentes da aplicação

--- src/pay-take.js
+++ src/pay-take.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
+
 import "./pay-take.css";
 
+//Components
+import Menu from "./components/menu/menu";
+import Produtos from "./components/produtos/produtos";
+import Checkout from "./components/checkout/checkout";
+
 function PayTake() {
+    //estados principáis da aplicação
     const [carrinho, setCarrinho] = useState({ produtos: [] });
     const [exibirProdutos, setExibirProdutos] = useState(true);
-    const [exibirCheckout, setExibirCheckout] = useState(true);
+    const [exibirCheckout, setExibirCheckout] = useState(false);
     const [total, setTotal] = useState("0,00");
 
     return (
         <div className="">
             <h1>Pay&#38;Take</h1>
+            <Menu />
+            <Produtos />
+            <Checkout />
         </div>
     );
 }
